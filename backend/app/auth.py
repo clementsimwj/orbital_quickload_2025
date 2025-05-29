e0d784e310f45d4106e9b1ba87c3cf3dbcb01e38
clementsimwj <clementsim333@gmail.com>
2025-05-29 23:49:27 +0800

    Created new branch feature-authentication

---
+++ backend/app/auth.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
+from configurations import users_collection
+
