600a4695184a48b78c5acf8f4a01a22736e9f582
clementsimwj <clementsim333@gmail.com>
2025-05-30 14:06:03 +0800

    Added CORS Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
 import auth
 from auth import get_current_user
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with specific domain in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 
 user_dependency = Annotated[dict, Depends(get_current_user)]
